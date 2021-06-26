0ed0fb2b129fbe4da6f0569462815b12fd1fdaba
Linda Giorgadze <linda.giorgadze17b@geolab.edu.ge>
2021-06-27 01:51:55 +0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Header from './components/Header';
+import TodoApp from './components/TodoApp';
+
+function App() {
+  return (
+    <div className="App">
+      <TodoApp />
+    </div>
+  );
+}
+
+export default App;
